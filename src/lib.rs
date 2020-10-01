0f22ec1ba56de994e56cfb20348f2b861a73915c
chatn0ir07 <lacojust@outlook.de>
2020-10-01 19:56:41 +0200

    just gimme my t-shirt :(

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,6 @@ pub mod sync {
         pub url: String,
         pub image_url: String,
         pub trailer_url: Option<String>,
-
-
         pub title: String,
         pub airing: bool,
         pub synopsis: Option<String>,
@@ -44,7 +42,45 @@ pub mod sync {
         }
     }
 
+    #[derive(serde::Deserialize, serde::Serialize)]
+    pub struct Manga {
+        pub url: String,
+        pub title: String,
+        pub title_english: String,
+        pub title_japanese: String,
+        pub image_url: String,
+        pub r#type: String,
+        pub volumes: u32,
+        pub chapters: u32,
+        pub synopsis: Option<String>,
+        pub score: f32,
+        pub scored_by: usize,
+    }
 
+    impl Manga {
+        pub fn from<T: Into<i32>>(id: T) -> Option<Manga> {
+            let req = reqwest::blocking::get(format!("https://api.jikan.moe/v3/manga/{}", id.into()).as_str());
+            match req {
+                Ok(x) => {
+                    let output = &x.text().unwrap();
+                    if let Ok(json) = serde_json::from_str(output) {
+                        Some(json)
+                    }else{
+                        None
+                    }
+                },
+                _ => {
+                    None
+                }
+            }
+        }
+    }
+
+    impl std::fmt::Debug for Manga {
+        fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            formatter.write_str(&serde_json::to_string(self).unwrap())
+        }
+    }
 
 }
 
--- src/test.rs
+++ src/test.rs
@@ -6,3 +6,10 @@ fn violet_evergarden_from_id() {
     let a: sync::Anime = sync::Anime::from(33352).expect("Violet Evergarden");
     assert_eq!(a.title, "Violet Evergarden");
 }
+
+#[test]
+fn monster_from_id() {
+    let m: sync::Manga = sync::Manga::from(1).expect("Monster");
+    assert_eq!(m.title, "Monster");
+
+}
